0e04a6f110880a69d82f9a830ebe9cb1be1133e5
Zombie1995 <egorkerber1995@mail.ru>
2026-05-08 20:59:26 +0300

    Revert "Update styles and fonts for improved aesthetics and readability; change header background color and adjust section styles."
    This reverts commit 208196e081ce9516034d96ec4d4e1d815ff635e7.

--- script.js
+++ script.js
@@ -108,9 +108,9 @@ document.addEventListener('DOMContentLoaded', function() {
     window.addEventListener('scroll', function() {
         const header = document.querySelector('.header');
         if (window.scrollY > 100) {
-            header.style.background = 'rgba(7, 7, 7, 0.90)';
+            header.style.background = 'rgba(255, 255, 255, 0.98)';
         } else {
-            header.style.background = 'rgba(7, 7, 7, 0.75)';
+            header.style.background = 'rgba(255, 255, 255, 0.95)';
         }
     });
 
